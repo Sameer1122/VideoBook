be1cd197e85fe2799a93edcce5fa9ef059779a81
Sameer1122 <sameer.ssj786@gmail.com>
2022-08-07 21:09:10 +0500

    fixed logo img

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href={"/"}>
-        <div className = 'w-[100px] md:w-[130px]'>
+        <div className = 'w-[50px] md:w-[70px]'>
             <Image src={Logo} alt = 'TikTik' layout='responsive' />
         </div>
         </Link>
