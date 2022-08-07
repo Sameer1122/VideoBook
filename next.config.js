b0693fdc2c29b298c76ca420f9aaf903a6657615
Sameer1122 <sameer.ssj786@gmail.com>
2022-08-07 19:04:58 +0500

    pushing it

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  images: {
+    domains: ['i.tribune.com.pk', 'lh3.googleusercontent.com'],
+  },
   swcMinify: true,
 }
 
---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next'
+import axios from 'axios'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Video } from '../types'
+import VideoCard from '../components/VideoCard'
+import { BASE_URL } from '../utils'
+import NoResults from '../components/NoResults'
+interface Iprops {
+  videos: Video[]
+}
+const Home = ({videos}:Iprops) => {
+  
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+        {videos.length ? (
+          videos.map((video: Video) => (
+            <VideoCard post = {video} key = {video._id} />
+          ))
+        ): (
+          <NoResults text = {'No Videos'} />
+        )}
+    </div>
+  )
+}
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
+
+  if(topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  }
+  
+  return {
+    props: { videos: response.data },
+  };
+};
+export default Home
